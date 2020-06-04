44dabc0848d4de9b99d74a2d887f5f21233f64f7
desmond <desmondgee00@gmail.com>
2020-06-04 02:56:26 -0700

    correct ridge algorithm in settings and adjusted terrain values

--- lib/dimension_online_web/game/noise_map.ex
+++ lib/dimension_online_web/game/noise_map.ex
@@ -10,18 +10,10 @@ defmodule DimensionOnlineWeb.Game.NoiseMap do
     column_text = for y <- 0..(width - 1) do
       row = for x <- 0..(width - 1) do
         height = calculate_height(x, y)
-        cond do
-          height < 0.2 ->
-            "<div class='level-1'></div>"
-          height < 0.4 ->
-            "<div class='level-2'></div>"
-          height < 0.6 ->
-            "<div class='level-3'></div>"
-          height < 0.8 ->
-            "<div class='level-4'></div>"
-          true ->
-            "<div class='level-5'></div>"
-        end
+        balanced_world(height)
+        # mountain_world(height)
+        # grass_world(height)
+        # water_world(height)
       end
 
       Enum.join(row)
@@ -30,16 +22,77 @@ defmodule DimensionOnlineWeb.Game.NoiseMap do
     Phoenix.HTML.raw(Enum.join(column_text, "<br/>"))
   end
 
+  def balanced_world(height) do
+    cond do
+      height < 0.2 ->
+        "<div class='level-1'></div>"
+      height < 0.4 ->
+        "<div class='level-2'></div>"
+      height < 0.6 ->
+        "<div class='level-3'></div>"
+      height < 0.8 ->
+        "<div class='level-4'></div>"
+      true ->
+        "<div class='level-5'></div>"
+    end
+  end
+
+  def mountain_world(height) do
+    cond do
+      height < 0.25 ->
+        "<div class='level-1'></div>"
+      height < 0.35 ->
+        "<div class='level-2'></div>"
+      height < 0.45 ->
+        "<div class='level-3'></div>"
+      height < 0.7 ->
+        "<div class='level-4'></div>"
+      true ->
+        "<div class='level-5'></div>"
+    end
+  end
+
+  def grass_world(height) do
+    cond do
+      height < 0.3 ->
+        "<div class='level-1'></div>"
+      height < 0.7 ->
+        "<div class='level-2'></div>"
+      height < 0.8 ->
+        "<div class='level-3'></div>"
+      height < 0.9 ->
+        "<div class='level-4'></div>"
+      true ->
+        "<div class='level-5'></div>"
+    end
+  end
+
+  def water_world(height) do
+    cond do
+      height < 0.5 ->
+        "<div class='level-1'></div>"
+      height < 0.7 ->
+        "<div class='level-2'></div>"
+      height < 0.8 ->
+        "<div class='level-3'></div>"
+      height < 0.9 ->
+        "<div class='level-4'></div>"
+      true ->
+        "<div class='level-5'></div>"
+    end
+  end
+
   @land_layer_setting NoiseLayerSetting.default()
   @dome_layer_setting %NoiseLayerSetting{NoiseLayerSetting.default() |
     offset_x: 44358461.74457203,
     offset_y: 91565620.6971831,
-    frequency: 0.032
+    frequency: 0.0082
   }
   @ridge_layer_setting %NoiseLayerSetting{NoiseLayerSetting.default() |
     offset_x: 3113267.54804393,
     offset_y: 5965100.813402789,
-    frequency: 0.032
+    frequency: 0.0031,
+    algorithm: :ridge
   }
 
   # TODO: Make the percentages of these vary along the surface to make exploring interesting.
@@ -48,10 +101,20 @@ defmodule DimensionOnlineWeb.Game.NoiseMap do
     dome_layer_height = calculate_layer_height(x, y, @dome_layer_setting)
     ridge_layer_height = calculate_layer_height(x, y, @ridge_layer_setting)
 
-    land_layer_height / 3 + land_layer_height * dome_layer_height / 3 + ridge_layer_height / 3
+    apply_weights([
+      {land_layer_height, 1},
+      {land_layer_height * dome_layer_height, 1.5},
+      {ridge_layer_height, 3.5}
+    ])
+
   end
 
-  def calculate_layer_height(x, y, setting) do
+  defp apply_weights(list) do
+    total_weight = Enum.reduce(list, 0, fn {_, weight}, acc -> acc + weight end)
+    Enum.reduce(list, 0, fn {layer_height, weight}, acc -> acc + layer_height *  (weight / total_weight) end)
+  end
+
+  defp calculate_layer_height(x, y, setting) do
     noise = Simplex.noise(x * setting.frequency + setting.offset_x, y * setting.frequency + setting.offset_y)
 
     layer_height = case setting.algorithm do
